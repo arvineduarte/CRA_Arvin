5d18dadeec6cdb8213e251fdc34022ced2af5139
arvineduarte <arvin.eduarte@tanaakk.co.jp>
2021-06-02 14:47:59 +0900

    4th commit Added the picture for Login page

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    construction_report(),
+    MyApp(),
   );
 }
 
-class construction_report extends StatelessWidget {
+class MyApp extends StatelessWidget {
   const construction_report({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Arvin',
       home: Scaffold(
         body: Container(
         decoration: BoxDecoration(
